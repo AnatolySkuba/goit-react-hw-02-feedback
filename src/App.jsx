9c6aac39bb5c4addcd48871b15a963b05a4907ca
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-05-28 15:43:52 +0300

    delete div in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,6 @@ import Section from 'components/Section/Section';
 
 export class App extends Component {
   render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback)" />
-      </div>
-    );
+    return <Section title="Please leave feedback)" />;
   }
 }
